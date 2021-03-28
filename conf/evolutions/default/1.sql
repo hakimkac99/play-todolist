be3e759caf78e5d044dd41b38877f82d59e754b1
hakimkac99 <kacemi.mohammed.abdelhakim@gmail.com>
2021-03-29 01:51:46 +0200

    :wrench: Adding ebean.properties

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,17 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table task (
+  id                            bigserial not null,
+  title                         varchar(255) not null,
+  done                          boolean default false not null,
+  constraint pk_task primary key (id)
+);
+
+
+# --- !Downs
+
+drop table if exists task cascade;
+
